895beb28587c994e63912f50dab0bd0254e2f437
Suchit Parmar <suchit.technopixar@gmail.com>
2023-12-25 10:25:38 +0530

    image set

--- src/app/Pages/Home/SuperAdmin/Service/service-list/service-list.component.spec.ts
+++ src/app/Pages/Home/SuperAdmin/Service/service-list/service-list.component.spec.ts
@@ -1,4 +1,4 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+ s import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ServiceListComponent } from './service-list.component';
 
